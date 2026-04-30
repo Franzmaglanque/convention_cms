853025e2fd25251f5ccda32691a8b93136e69e74
Franz Maglanque <fcmaglanque@puregold.com.ph>
2026-04-30 13:52:29 +0800

    changed login api

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
   const onSubmit = async (values: LoginInput) => {
     setLoading(true);
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/cms/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
